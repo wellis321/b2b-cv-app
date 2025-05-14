bfc6be8988508d81902571de03f9caec02a48598
wellis321 <wellis321@msn.com>
2025-05-14 17:23:51 +0100

    changed runtime to edge

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter({
-            // Use the correct runtime format for Vercel
-            runtime: 'nodejs18.x',
+            // Use the Edge runtime which is more compatible
+            runtime: 'edge',
             // Enable regional edge functions
             regions: ['iad1']
         })
