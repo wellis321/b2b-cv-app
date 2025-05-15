0cee9279f9877583c9f5c5afc8874b0bf6389e4f
wellis321 <wellis321@msn.com>
2025-05-15 21:35:44 +0100

    removed pdf button

--- src/routes/cv/@[username]/+page.svelte
+++ src/routes/cv/@[username]/+page.svelte
@@ -285,27 +285,6 @@
 								</div>
 							{/if}
 						</div>
-
-						<div class="mt-8 print:hidden">
-							<button
-								onclick={handlePrint}
-								class="inline-flex items-center gap-2 rounded bg-white px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-50"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="h-4 w-4"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M5 4v3H4a2 2 0 00-2 2v3a2 2 0 002 2h1v2a2 2 0 002 2h6a2 2 0 002-2v-2h1a2 2 0 002-2V9a2 2 0 00-2-2h-1V4a2 2 0 00-2-2H7a2 2 0 00-2 2zm8 0H7v3h6V4zm0 8H7v4h6v-4z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								View PDF Version
-							</button>
-						</div>
 					</div>
 
 					<!-- Profile photo -->
