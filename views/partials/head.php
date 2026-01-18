c1a895c8c8a531315cca42b9851541235ee4a933
William Ellis <wellis321@msn.com>
2026-01-18 20:55:07 +0000

    new favicons

--- views/partials/head.php
+++ views/partials/head.php
@@ -39,8 +39,12 @@ if ($breadcrumbs) {
 outputStructuredData($schemas);
 ?>
 
-<link rel="icon" type="image/png" href="/static/favicon.png">
-<link rel="icon" type="image/svg+xml" href="/static/favicon.svg">
+<!-- Favicons -->
+<link rel="apple-touch-icon" sizes="180x180" href="/static/images/favicon_io/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon_io/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon_io/favicon-16x16.png">
+<link rel="manifest" href="/static/images/favicon_io/site.webmanifest">
+<link rel="shortcut icon" href="/static/images/favicon_io/favicon.ico">
 <style>
 /* Accessibility: Skip to main content link */
 .sr-only {
